79f455b4b2414c21c1994577010cedfeca7e6d6e
brisa-pedronetto <pedro@brisa.is>
2020-09-10 22:50:17 +0000

    IMproved version

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 (() => {
   const container = document.querySelector(".container");
+  const inner = document.querySelector(".inner");
 
   const firstSlide = container.querySelector(".slide:first-child");
   const firstSlideContent = firstSlide.querySelector(".content > div");
@@ -7,8 +8,6 @@
   const secondSlide = container.querySelector(".slide:last-child");
   const secondSlideImage = secondSlide.querySelector(".image");
   const secondSlideContent = secondSlide.querySelector(".content > div");
-
-  let slideHeight;
 
   function getScrollFromTop() {
     return window.pageYOffset !== undefined
@@ -20,68 +19,53 @@
   function setupContainer() {
     slideHeight = secondSlide.offsetHeight;
     firstSlide.style.top = container.offsetTop;
-    secondSlideImage.style.transform = `translateX(100%)`;
   }
 
   function updateContainer() {
     if (
       getScrollFromTop() >= container.offsetTop &&
-      getScrollFromTop() <= slideHeight + container.offsetTop
+      getScrollFromTop() <=
+        container.offsetHeight - slideHeight + container.offsetTop
     ) {
       // While scrolling through effect region
 
       // Percentage of the scrolling region through which
       // the user has revealed
       percRegionScrolled =
-        ((getScrollFromTop() - container.offsetTop) / slideHeight) * 100;
+        ((getScrollFromTop() - container.offsetTop) /
+          (container.offsetHeight - slideHeight)) *
+        100;
 
       // Make first slide fixed
-      firstSlide.classList.add("fixed");
-      firstSlide.classList.remove("absolute");
-
-      // Make image and content from second slide fixed so that
-      // they can be controlled by JS to create the effect
-      secondSlideImage.classList.add("fixed");
-      secondSlideContent.classList.add("fixed");
+      inner.classList.add("fixed");
+      inner.classList.remove("stay");
 
       // Show/hide text contents
-      if (percRegionScrolled > 40) {
+      if (percRegionScrolled > 5) {
         firstSlideContent.classList.add("hide");
+        secondSlideContent.classList.remove("hide");
       } else {
         firstSlideContent.classList.remove("hide");
+        secondSlideContent.classList.add("hide");
       }
-
-      // Reveal the second image (decrease translateX on the way down)
-      const revealAmount = 100 - percRegionScrolled * 1.8;
-      secondSlideImage.style.transform = `translateX(${
-        revealAmount < 0 ? 0 : revealAmount
-      }%)`;
 
       // Give the second image some time to
       // relocate before the transition kicks in
-      if (percRegionScrolled < 5) {
-        secondSlideImage.style.transition = "none";
+      if (percRegionScrolled <= 0) {
+        secondSlideImage.classList.remove("reveal");
       } else {
-        secondSlideImage.style.transition = null;
+        secondSlideImage.classList.add("reveal");
       }
+    } else if (getScrollFromTop() > slideHeight + container.offsetTop) {
+      // Below effect region
+
+      inner.classList.add("stay");
     } else {
-      // While scrolling thorough other regions
+      // Above effect region
 
       // Release first slide to go with the document's flow
-      firstSlide.classList.add("absolute");
-      firstSlide.classList.remove("fixed");
-
-      // Release second slide to follow the document's flow
-      secondSlideContent.classList.remove("fixed");
-      secondSlideImage.classList.remove("fixed");
-      secondSlideImage.style.transform = null;
-    }
-
-    // Show the second image after it's been scrolled over' (32 is a slack)
-    if (getScrollFromTop() >= container.offsetTop + slideHeight - 32) {
-      secondSlideImage.classList.add("stay");
-    } else {
-      secondSlideImage.classList.remove("stay");
+      inner.classList.remove("fixed");
+      inner.classList.remove("stay");
     }
   }
 
